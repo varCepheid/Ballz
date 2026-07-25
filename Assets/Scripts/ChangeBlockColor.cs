65523c6b8a35b032f0c1af906d2a2445f327532e
Alex <avhoi83@gmail.com>
2026-07-25 16:09:43 -0700

    adjusted block color scheme

--- Assets/Scripts/ChangeBlockColor.cs
+++ Assets/Scripts/ChangeBlockColor.cs
@@ -7,7 +7,9 @@ public class ChangeBlockColor : MonoBehaviour
   public SpriteRenderer[] sprites;
   public UpdateNumberOnBlock numberGetter;
   private int number;
-  private float red, green, blue;
+  private float red, green, blue, convertedNumber;
+  private readonly float SCALE_FACTOR = (float)Math.PI / 128.0f * (256f / 100f);
+  private readonly float MIN_VALUE = 3f / 16f;
   private Color color;
 
   // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,10 +23,11 @@ public class ChangeBlockColor : MonoBehaviour
   void Update()
   {
     // create color based on number on block
-    number = numberGetter.number % 256;
-    red = Math.Max(0, 3f * Math.Abs(number - 128f) - 128f);
-    green = Math.Max(0, 256f - 3f * Math.Abs(number - (512f / 3f)));
-    blue = Math.Max(0, 256f - 3f * Math.Abs(number - (256f / 3f)));
+    number = numberGetter.number % 100;
+    convertedNumber = number * SCALE_FACTOR;
+    red = (float)Math.Max(MIN_VALUE, 2f / 3f * Math.Cos(convertedNumber) + 1f / 3f);
+    green = (float)Math.Max(MIN_VALUE, 2f / 3f * Math.Cos(convertedNumber - Math.PI * 2f / 3f) + 1f / 3f);
+    blue = (float)Math.Max(MIN_VALUE, 2f / 3f * Math.Cos(convertedNumber + Math.PI * 2f / 3f) + 1f / 3f);
     color = new(red, green, blue);
 
     foreach (SpriteRenderer sprite in sprites) // color each component of block
